1ce8100d875334ff33696925e181a61aac702df7
Sanghyun Yi <njsharry32@gmail.com>
2022-07-13 19:58:31 +0900

    chapter11-question4-solved-chapter11-clear

--- src/chapter11/question4.java
+++ src/chapter11/question4.java
@@ -3,34 +3,33 @@ package chapter11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class question4 {
-    static int N;
-    static boolean [] used_coin;
+    static int N, possible;
+    static int [] coins;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        used_coin = new boolean[N];
-        int [] coins = new int [N];
+        coins = new int [N];
         for(int i=0;i<N;i++) {
             coins[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(coins);
-        int amount = 1;
-        while(true) {
-            if(!match_amount(amount)) {
+        if(coins[0]!=1) {
+            System.out.println("1");
+            return;
+        }
+        possible = coins[0];
+        for(int i=1;i<N;i++) {
+            if(coins[i]>possible+1) {
                 break;
             }
-            amount++;
+            possible += coins[i];
         }
-        System.out.println(amount);
+        System.out.println((possible+1));
         br.close();
-    }
-    static boolean match_amount(int a) {
-        return true;
     }
 }
