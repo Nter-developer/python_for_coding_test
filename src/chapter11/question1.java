9457de222e227c43a2898f1117634c16c1a9d910
Sanghyun Yi <njsharry32@gmail.com>
2022-07-14 07:33:10 +0900

    chapter11-code-refactoring

--- src/chapter11/question1.java
+++ src/chapter11/question1.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-
 public class question1 {
-    static int N;
+    static int N, G = 0;
     static int [] argonaut;
-    static int G = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -20,33 +19,15 @@ public class question1 {
         for(int i=0;i<N;i++) {
             argonaut[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=1;i<=N;i++) {
-            for(int j=0;j<N;j++) {
-                if(argonaut[j]==i) {
-                    //System.out.println("searching partner with "+j+"th arg, fears "+argonaut[j]);
-                    recruit(argonaut[j]);
-                }
+        Arrays.sort(argonaut);
+        int grouped = 0;
+        for(int i=0;i<N;i++) {
+            if(i-grouped>=argonaut[i]) {
+                grouped = i;
+                G++;
             }
         }
         System.out.println(G);
         br.close();
-    }
-    static void recruit(int f) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<N;i++) {
-            if(argonaut[i]<=f) {
-                //System.out.println("find partner "+i);
-                list.add(i);
-            }
-            if(list.size()==f) {
-                //System.out.println("find complete");
-                for(int k: list) {
-                    //System.out.println(k+" got in party");
-                    argonaut[k] = N+1;
-                }
-                G++;
-                break;
-            }
-        }
     }
 }
